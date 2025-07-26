f8c836a346e78ba1dedaff879497434127001a1f
allenjose24 <allenjose2110@gmail.com>
2025-07-27 00:51:25 +0530

    Added Fxn's for Time data members(joinedAt, updatedAt)

--- src/main/java/com/fitness/FitnessClubApp/repository/MemberRepository.java
+++ src/main/java/com/fitness/FitnessClubApp/repository/MemberRepository.java
@@ -3,12 +3,17 @@ package com.fitness.FitnessClubApp.repository;
 import com.fitness.FitnessClubApp.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
     Optional<Member> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    List<Member> findAllByJoinDateAfterOrderByJoinDateDesc(LocalDateTime since);
+    List<Member> findAllByUpdatedAtAfterOrderByUpdatedAtDesc(LocalDateTime since);
 
 
 }
