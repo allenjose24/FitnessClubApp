fb1e3ecbc177cf88e3440ee10753a696e2d29700
allenjose24 <allenjose2110@gmail.com>
2025-08-01 15:41:16 +0530

    Added Transaction Entity

---
+++ src/main/java/com/fitness/FitnessClubApp/model/Transaction.java
@@ -0,0 +1,43 @@
+package com.fitness.FitnessClubApp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "transactions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "plan_id")
+    private Plan plan;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type; // NEW_PURCHASE, UPGRADE, RENEWAL
+
+    private BigDecimal amount;
+
+    private LocalDateTime timestamp;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
+
