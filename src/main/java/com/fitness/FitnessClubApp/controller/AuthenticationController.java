22fb46606ee19f2935c9b2cbb389a942dfdbc6d8
allenjose24 <allenjose2110@gmail.com>
2025-08-01 15:32:10 +0530

    Updated the HttpStatus code

--- src/main/java/com/fitness/FitnessClubApp/controller/AuthenticationController.java
+++ src/main/java/com/fitness/FitnessClubApp/controller/AuthenticationController.java
@@ -39,7 +39,7 @@ public class AuthenticationController {
             AuthenticationResponse response = registrationService.register(registrationDTO, Role.TRAINER);
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
         } catch(Exception e){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
     }
 
